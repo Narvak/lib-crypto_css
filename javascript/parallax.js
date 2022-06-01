4f19042392b73048e44418c17ffd513c7dbee990
Narvak <91684269+Narvak@users.noreply.github.com>
2022-06-01 14:37:01 +0200

    Add files via upload

---
+++ javascript/parallax.js
@@ -0,0 +1,3 @@
+/* paralax ../medias/images/image_paralax.png */
+    /* link : https://pixelcog.github.io/parallax.js/#:~:text=js-,Parallax.,on%20GitHub%20Check%20out%20v2.*/
+    $('.parallax-window').parallax({imageSrc: '../medias/images/image_paralax.png'});
