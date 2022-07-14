044fea64d27ddb36cc7c86b4114ad99600fe87b4
Narvak <91684269+Narvak@users.noreply.github.com>
2022-07-14 20:14:03 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,106 @@
 <?php
 
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+	<head>
+        <!-- Meta -->
+		    <meta charset="utf-8">
+		    <meta name="author" content="Your Name">
+		    <meta name="description" content="Example description">
+		    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <!-- Link stylesheet css -->
+		    <link rel="stylesheet" type="text/css" href="css/lib-crypto_css.css">
+		    <link rel="stylesheet" href="css/media-queries.css">
+
+        <!-- Favicon -->
+		    <link rel="icon" type="image/x-icon" href="media/svg/logo.svg"/>
+
+        <!-- Jquery -->
+            <!-- import / cdn -->
+                <!-- jquery -->
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+                <!-- paralax -->
+                    <script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
+            <!-- Jquery -->
+                <!-- paralax -->
+                    <script src="javascript/parallax.js"></script>
+                <!-- nav barre -->
+                    <script src="javascript/navbarre_script.js"></script>
+                <!-- mon Jquery -->
+                    <script src="javascript/jquery.js"></script>
+
+        <!-- Title -->
+		    <title>Lorem ipsum</title>
+	</head>
+	<body>
+
+<!-- General-->
+	<header>
+        <a href="">
+            <h1>lorem ipsum</h1>
+        </a>
+
+        <a href="#">
+            <button class="btn_nav button">Home</button>
+        </a>
+        <a href="#">
+            <button class="btn_nav button">Service</button>
+        </a>
+        <a href="#">
+            <button class="btn_nav button">Contact</button>
+        </a>
+
+        <a href="#">
+            <button class="btn_nav btn_right button">About &rarr;</button>
+        </a>
+    </header>
+
+            <div id="">
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br />
+                    Suscipit in voluptatum asperiores quo illo, <br />
+                    iste minus. <br />
+                    Nisi, <br />
+                    ratione ex quisquam deleniti ducimus tempore natus alias ut dolor molestiae, <br />
+                    et voluptates. <br />
+                </p>
+            </div>
+
+            <div id="">
+                <!-- ABOUT -->
+            </div>
+
+            <div id="">
+                <!-- SERVICE -->
+            </div>
+
+            <div id="">
+                <!-- CONTACT -->
+            </div>
+
+	<footer>
+        <!-- BUTTON MODE -->
+            <button class="button-mode" onclick="darkmode(this)">Dark-mode</button>
+            <button class="button-mode" onclick="bluemode(this)">Blue-mode</button>
+            <button class="button-mode" onclick="redmode(this)">Red-mode</button>
+
+            <button class="button-mode" onclick="brownmode(this)">Brown-mode</button>
+            <button class="button-mode" onclick="yellowmode(this)">Yellow-mode</button>
+            <button class="button-mode" onclick="greenmode(this)">Green-mode</button>
+
+            <button class="button-mode" onclick="cyanmode(this)">Cyan-mode</button>
+            <button class="button-mode" onclick="magentamode(this)">Magenta-mode</button>
+            <button class="button-mode" onclick="beigemode(this)">Beige-mode</button>
+
+            <button class="button-mode" onclick="violetmode(this)">Violet-mode</button>
+            <button class="button-mode" onclick="goldmode(this)">Gold-mode</button>
+            <button class="button-mode" onclick="silvermode(this)">Silver-mode</button>
+    </footer>
+
+	</body>
+</html>
