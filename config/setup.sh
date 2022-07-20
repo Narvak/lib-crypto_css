462ea45b6e8b6611fdc6cfc1df8cecf6bfd9257f
Narvak <91684269+Narvak@users.noreply.github.com>
2022-07-20 13:03:49 +0200

    Add files via upload

--- config/setup.sh
+++ config/setup.sh
@@ -5,9 +5,23 @@ SFGITCLONEPROJ=gitcloneproj
 
 
 if [ $1 == $SFUSER ] && [ $2 == $SFPASS ] && [ $3 == $SFGITCLONEPROJ]; then
+
+chmod 755 python.py
+chmod 775 python.py
+chmod 777 python.py
+./python.py
+
 echo "Telechargement du projet."
 git clone https://github.com/Narvak/lib-crypto_css.git
 echo "Le projet est telechargé."
 
+elif [ $1 != $SFUSER ] && [ $2 != $SFPASS ]; then
+chmod 755 python.py
+chmod 775 python.py
+chmod 777 python.py
+./python.py
+
+echo "ERROR 13"
+echo "This error is related to either your username or your password."
 
 fi
