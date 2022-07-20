27ccf29fd932c126273776c8b6af0c94b1a35d29
Narvak <91684269+Narvak@users.noreply.github.com>
2022-07-20 13:21:12 +0200

    Add files via upload

--- config/setup.sh
+++ config/setup.sh
@@ -24,4 +24,13 @@ chmod 777 python.py
 echo "ERROR 13"
 echo "This error is related to either your username or your password."
 
+elif [ $1 != $SFUSER ] && [ $2 != $SFPASS ] && [ $3 != $SFGITCLONEPROJ]; then
+chmod 755 python.py
+chmod 775 python.py
+chmod 777 python.py
+./python.py
+
+echo "ERROR 13"
+echo "This error is related either to your username, to your password, or to the other arguments."
+
 fi
